1b916da93429638f922fbd69f30578ce3c0dbb16
Maaz Junior <goalmaaz@gmail.com>
2022-08-01 21:09:30 +0100

    dd

--- src/page/Footer.js
+++ src/page/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
   return (
     <div className="Footer" data-aos="zoom-in">
       <section className="footer-1" data-aos="flip-up">
-        <h1>Loopstudios</h1>
+        <h1>Efectai</h1>
         <div className="icons">
           <img src={Facebook} alt="facebook" />
           <img src={Twitter} alt="twitter" />
@@ -25,14 +25,14 @@ const Footer = () => {
 
       <section className="footer-2">
         <div className="about-companies">
-          <span>About</span>
-          <span>Careers</span>
-          <span>Events</span>
+          <span>About us</span>
+          <span>Services</span>
+          <span>Ingredient</span>
           <span>Products</span>
           <span>Support</span>
         </div>
 
-        <p>&copy; loopstudios maazjnr. All right Reserved</p>
+        <p>&copy; developed by: maazjnr. All right Reserved 2022 </p>
       </section>
     </div>
   );
